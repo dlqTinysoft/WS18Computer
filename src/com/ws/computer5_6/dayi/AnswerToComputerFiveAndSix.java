1029935099bf056e41f8717d5fb3b6a6d086e973
dongleqiang <dlq_tinysoft@163.com>
2020-04-06 15:58:18 +0800

    computerToFourAndThree

---
+++ src/com/ws/computer5_6/dayi/AnswerToComputerFiveAndSix.java
@@ -0,0 +1,165 @@
+package com.ws.computer5_6.dayi;
+import java.util.Arrays;
+import java.util.Iterator;
+/**
+ * 
+ * @author dlq_tinysoft
+ * 
+ *         1. 环境变量的配置：java.exe 和javac.exe 所在的路径：C:\Program
+ *         Files\Java\jdk1.8.0_202\bin 配置到环境变量path里面。
+ * 
+ *         2.javac 编译 .java文件 编译成我们的 .class文件 . java命令把我们的.class 加载到jvm里面并运行。
+ * 
+ *         3. jdk > jre > jvm
+ * 
+ *         4. 常量 和 变量 重点：变量
+ * 
+ *         5.条件选择语句 if 和switch
+ * 
+ *         6.循环： while do-while for:增强for循环
+ *         
+ *         7.数组： 数组的定义 数组的使用 数组的遍历
+ */
+public class AnswerToComputerFiveAndSix {
+
+	public static void main(String[] args) {
+		// 常量：1.final 2.只能被赋值一次
+		final int age = 100;
+		System.out.println(age);
+		// 变量： 1. 基本类型的变量 和引用类型的变量
+		// 基本类型变量： byte char boolean short int long float double
+		// 定义一个整型变量: 变量类型 变量名字 变量的值
+		// 标识符： 字母 数字 下划线组成， 但是不能以数字开头
+		int myInt = 100;
+		System.out.println(myInt);
+		// 定义了double类型变量
+		// 局部变量要使用 必须先初始化，否则无法使用，编译通过不了
+		double myDouble = 3.3;
+		System.out.println(myDouble);
+		// 引用类型变量
+		String str = "ddddd";
+		System.out.println(str);
+		//MethodInfo();
+		//methodFor();
+		methodArray();
+	}
+
+	//数组的定义 数组的使用 数组的遍历 
+	public static void methodArray() {
+		
+		//定义一个整型数组
+		//hash地址
+		int[] myIntArray = new int[3];
+		System.out.println(myIntArray);
+		
+		for(int i = 0 ; i<myIntArray.length ; i++){
+			myIntArray[i] = i+1;
+		}
+		
+		for(int j = 0 ; j < myIntArray.length ; j++){
+			System.out.println(myIntArray[j]);
+		}
+
+		//增强for循环来遍历数组
+		String[] myStr = { "abc", "cdf", "efg" };
+
+		for (int i1 = 0; i1 < myStr.length; i1++) {
+			System.out.println(myStr[i1]);
+		}
+
+		for (String str : myStr) {
+			System.out.println(str);
+		}
+		
+		String [] myStr1 = {"abc","efg"};
+		Arrays.toString(myStr1);
+		
+		
+		//javac 编译 是通过的
+		System.out.println(myStr1[2]);
+		
+		
+		
+	}
+
+	
+
+	// 循环
+	private static void methodFor() {
+		int i = 5;
+
+		while (i > 0) {
+			i--;
+			System.out.println(i);
+		}
+
+		do {
+			i--;
+			System.out.println(i);
+		} while (i > 0);
+
+		for (int j = 0; j <= 5; j++) {
+			System.out.println(j);
+		}
+
+		// 增强for循环
+
+		String[] myStr = { "abc", "cdf", "efg" };
+
+		for (int i1 = 0; i1 < myStr.length; i1++) {
+			System.out.println(myStr[i1]);
+		}
+
+		for (String str : myStr) {
+			System.out.println(str);
+		}
+
+	}
+
+	// if 和 switch
+	private static void MethodInfo() {
+
+		int a = 10;
+
+		int b = 4;
+
+		int c = 0;
+
+		// 第一种
+		if (true) {
+			c = a + b;
+		}
+
+		// 第二种
+		if (a > b) {
+			c = a - b;
+		} else {
+			c = a + b;
+		}
+
+		// 第三种
+		if (a > b) {
+
+		} else if (c > b) {
+
+		} else if (a > c) {
+
+		} else {
+
+		}
+
+		// =============switch============
+		// int String char
+		switch (1) {
+		case 1:
+			c = a + b;
+			break;
+		case 2:
+			c = a - b;
+		default:
+			c = 5;
+		}
+
+	}
+
+}
---
+++ src/com/ws/computer5_6/dayi/AnswerToComputerThreeAndFour.java
@@ -0,0 +1,170 @@
+package com.ws.computer5_6.dayi;
+
+import java.util.*;
+
+/**
+ * 
+ * @author dlq_tinysoft
+ * 
+ * 1. 配置环境变量：C:\Program Files\Java\jdk1.8.0_202\bin 添加到path下面。
+ * 
+ * 2. javac:编译  java是运行。  .java文件 使用javac编译成.class文件， 
+ *    java：把我们编译过的.class文件加载到jvm里面，并运行。
+ *    
+ * 3.jdk  jre  jvm  三者的关系
+ *   jdk：java开发包
+ *   jre:java运行的时候所依赖的环境
+ *   jvm:java程序都跑在java虚拟机里面。
+ *   jdk > jre > jvm
+ *   
+ *  4. 常量和变量
+ *     重点：变量
+ *     
+ *  5.条件语句： if家族 和switch家族
+ *  
+ *  6.循环： while do-while for：增强for循环
+ *  
+ *  7.数组： 数组的定义  数组的初始化  数组的遍历 数组的使用
+ */
+public class AnswerToComputerThreeAndFour {
+	
+	   public static void main(String[] args) {
+		   
+		   /**-------------------------变量复习------------------*/
+		   //定义一个整型变量
+		   //变量类型： int   变量的名字：myInt,标识符只能有数字 字母 和下划线组成，不能以数字开头
+		   //变量的值：100
+		   int myInt = 100 ;
+		   //变量类型： 基本数据类型和引用类型  
+		   //基本数据类型有八种：byte boolean char short int long float double 
+		   System.out.println(myInt);
+		   byte myByte = 2 ;
+		   System.out.println(myByte);
+		   //字符串类型,引用类型的变量
+		   String str = "abc";
+		   System.out.println(str);
+		   //局部变量必须先初始化，才能使用。成员变量可以不初始化，系统会给一个默认值。
+		   double myDouble = 2.3 ;
+		   System.out.println(myDouble);
+		   
+		   float myFloat = (float) 3.2 ;
+		   float myFloat1 = 3.2f;
+		   double myDouble1 = 3.2;
+		   System.out.println(myFloat);
+		   System.out.println(myFloat1);
+		   System.out.println(myDouble1);
+		   
+		   
+		   /**----------------常量复习-------------------*/
+		   //定义一个整型常量:只能被赋值一次的变量，叫做常量。 final 数据类型 
+		   final int myIntConstant = 100 ;
+		   System.out.println(myIntConstant);
+		   //字符串常量
+		   final String myStrConstant = "abc";
+		   System.out.println(myStrConstant);
+		   
+		   
+		   /**-----------------条件选择语句复习----------------------*/
+		   //methodIf();
+		   /**-----------------循环语句复习----------------------*/
+		   //methodFor();
+		   /**-----------------数组复习----------------------*/
+		   methodArray();
+	}
+	 //数组的复习
+	private static void methodArray() {
+		//定义一个大小为3的整型数组
+		//hash地址
+		int[] myIntArray = new int[3];
+		System.out.println(myIntArray);
+		//注意：数组的下标是从0开始的
+		for(int i = 0 ; i<myIntArray.length ; i++){
+			myIntArray[i] = i+1;
+		}
+		
+		for(int j = 0 ; j < myIntArray.length ; j++){
+			System.out.print(myIntArray[j]+" ");
+		}
+		
+		//定义并初始化
+		String[] myStrArray = {"abc","ddd","dddd"};
+		for(String str : myStrArray){
+			System.out.println(str);
+		}
+		
+		String[] myStrArray1 = new String[]{"ddd","dd","efg"};
+		System.out.println(myStrArray1);
+		
+		
+		
+	}
+
+	//循环语句
+    private static void methodFor() {
+		
+    	int i = 5 ;
+    	
+    	while( i > 0 ){
+    		System.out.println(i--);
+    	}
+    	
+    	do{
+    		System.out.println(i--);
+    	}while( i > 0 );
+    	
+    	for(int j = 0 ; j < 5 ; j++){
+    		System.out.println(j);
+    	}
+    	
+    	String[] strArray = {"abc","efg","www"};
+    	
+    	for(String str: strArray){
+    		System.out.println(str);
+    	}
+	}
+	//if家族和switch 
+	private static void methodIf() {
+		
+		//if家族总共有三种
+		
+		//第一种
+		
+		int a = 3 ;
+		int b = 4 ;
+		int c = 5 ;
+		
+		if(a > b){
+			c = a + b;
+		}
+		
+		//第二种
+		if(c > a ){
+			c = a - b;
+		}else{
+			c = a + b ;
+			
+		}
+		
+		//第三种
+		if( a > b ){
+			c = a + b ;
+		}else if(a > c){
+			c = a - b;
+		}else if(c > b){
+			c = a * b;
+		}else{
+			c = a / b ;
+		}
+		
+		//switch
+		//数据类型，int String char 
+		switch(a){
+		case 1 : c = a + b ;
+		break;
+		case 2 : c = a * b ;
+		case 3 : c = a / b ;
+		case 4 : c = a - b;
+		default: c = 0 ;
+		}
+	}
+}
